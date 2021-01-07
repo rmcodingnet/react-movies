73fe88bbfa4c0c496b421ce2265e8d70c1292992
rorymccann95 <rorymccann95@users.noreply.github.com>
2021-01-07 15:20:27 +0000

    Added missing fields to add form

--- src/components/NewMovie/NewMovie.js
+++ src/components/NewMovie/NewMovie.js
@@ -4,9 +4,27 @@ const { v4: uuidv4 } = require('uuid');
 
 const NewMovie = ({ addNewMovie, history }) => {
     const [values, setValues] = useState({})
+    const [actors, setActors] = useState([])
+    const [genres, setGenres] = useState([])
 
     const handleChangeValues = (newValue) => {
         setValues({ ...values, ...newValue });
+    }
+
+    const handleActorChange = (e) => {
+        e.preventDefault()
+        let valToAddToArray = values.actor;
+        actors.push(valToAddToArray);
+        values.actor = "";
+        setActors(actors)
+    }
+
+    const handleGenreChange = (e) => {
+        e.preventDefault()
+        let valToAddToArray = values.genre;
+        genres.push(valToAddToArray);
+        values.genre = "";
+        setGenres(genres)
     }
 
     const handleSubmit = (e) => {
@@ -14,12 +32,16 @@ const NewMovie = ({ addNewMovie, history }) => {
         const newElem = {
             id: uuidv4(), // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
             title: values.title,
+            posterurl: values.posterurl,
+            storyline: values.storyline,
+            actors: actors,
+            genres: genres,
             releaseDate: values.releaseDate,
-
+            imdbRating: values.imdbRating
         }
-        addNewMovie(newElem)
+        addNewMovie(newElem);
         console.log(newElem);
-        redirectWithId(newElem.id)
+        redirectWithId(newElem.id);
 
     }
 
@@ -32,11 +54,28 @@ const NewMovie = ({ addNewMovie, history }) => {
         <div className="form-container">
             <form className="form" onSubmit={(e) => handleSubmit(e)}>
                 <label>Movie Title</label>
-                <input type="text" value={values.title} onChange={(e) => handleChangeValues({ title: e.target.value })}></input>
-                <br/>
+                <input type="text" value={values.title} onChange={(e) => handleChangeValues({ title: e.target.value })} />
+                <br />
+                <label>URL for poster</label>
+                <input type="text" value={values.posterurl} onChange={(e) => handleChangeValues({ posterurl: e.target.value })} />
+                <br />
+                <label>Plot</label>
+                <textarea type="text" value={values.storyline} onChange={(e) => handleChangeValues({ storyline: e.target.value })}/>
+                <br />
+                <label>Actors</label>
+                <input type="text" value={values.actor} onChange={(e) => handleChangeValues({ actor: e.target.value })}/>
+                <button onClick={(e) => handleActorChange(e)}>Add Actor</button>
+                <br />
+                <label>Genres</label>
+                <input type="text" value={values.genre} onChange={(e) => handleChangeValues({ genre: e.target.value })}/>
+                <button onClick={(e) => handleGenreChange(e)}>Add Genre</button>
+                <br />
                 <label>Release Date</label>
-                <input type="text" value={values.releaseDate} onChange={(e) => handleChangeValues({ releaseDate: e.target.value })}></input>
-                <br/>
+                <input type="text" value={values.releaseDate} onChange={(e) => handleChangeValues({ releaseDate: e.target.value })} />
+                <br />
+                <label>Rating </label>
+                <input type="number" min="0" max="10" value={values.imdbRating} onChange={(e) => handleChangeValues({ imdbRating: e.target.value })} />
+                <br />
                 <button type="submit">Add Movie</button>
             </form>
         </div>

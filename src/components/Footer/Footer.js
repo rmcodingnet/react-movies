58f678f9f4ba8c6fed2f4ba7c867e87d332581ed
rorymccann95 <rorymccann95@users.noreply.github.com>
2021-01-07 12:09:00 +0000

    Added Footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <p>All Information displayed on this site is copyright to the Movie Industry and there respective publishing companies</p>
+        </div>
+    );
+};
+
+export default Footer;

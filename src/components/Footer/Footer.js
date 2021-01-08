bb9e669208afcda30ea0f5cd0085583a6de1e154
rorymccann95 <rorymccann95@users.noreply.github.com>
2021-01-08 16:30:46 +0000

    removed unnecessary captalization

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css';
 const Footer = () => {
     return (
         <div className="footer">
-            <p>All Information displayed on this site is copyright to the Movie Industry and there respective publishing companies</p>
+            <p>All Information displayed on this site is copyright to the movie industry and there respective publishing companies</p>
         </div>
     );
 };

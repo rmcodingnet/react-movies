2f55d2021937f5975748d974c1e030a280ad03db
rorymccann95 <rorymccann95@users.noreply.github.com>
2021-01-11 09:31:38 +0000

    removed unnecessary states and code

--- src/components/UpdateForm/UpdateForm.js
+++ src/components/UpdateForm/UpdateForm.js
@@ -4,13 +4,9 @@ import './UpdateForm.css'
 
 const UpdateForm = ({ movie, updateMovie, history }) => {
     const [values, setValues] = useState({});
-    const [actors, setActors] = useState([]);
-    const [genres, setGenres] = useState([]); 
 
     useEffect(() => {
-        setValues(movie)
-        setActors(movie.actors)
-        setGenres(movie.genres)
+        setValues(movie)        
     }, [movie])
 
     const handleChangeValues = (newValue) => {
@@ -18,15 +14,13 @@ const UpdateForm = ({ movie, updateMovie, history }) => {
     }
 
     const handleActorChanges = (newVal, pos) => {
-        actors.splice(pos,1,newVal);
-        setActors(actors);
-        setValues({...values, ...actors});
+        values.actors.splice(pos,1,newVal);
+        setValues({...values, ...values.actors});
     }
 
     const handleGenreChanges = (newVal, pos) => {
-        genres.splice(pos,1,newVal);
-        setGenres(genres);
-        setValues({...values, ...genres});
+        values.genres.splice(pos,1,newVal);
+        setValues({...values, ...values.genres});
     }
 
     const handleSubmit = (e) => {
@@ -48,7 +42,7 @@ const UpdateForm = ({ movie, updateMovie, history }) => {
                 <textarea type="text" value={values.storyline || ""} onChange={(e) => handleChangeValues({ storyline: e.target.value })} />
                 <br />
                 <label>Actors</label>
-                {actors ? actors.map((actor, index) => {
+                {values.actors ? values.actors.map((actor, index) => {
                     return (
                         <>
                             <input key={index} type="text" value={actor || ""} onChange={(e) => handleActorChanges(e.target.value,index)} />
@@ -57,7 +51,7 @@ const UpdateForm = ({ movie, updateMovie, history }) => {
                     )
                 }) : null}
                 <label>Genres</label>
-                {genres ? genres.map((genre, index) => {
+                {values.genres ? values.genres.map((genre, index) => {
                     return (
                         <>
                             <input key={index} type="text" value={genre || ""} onChange={(e) => handleGenreChanges(e.target.value,index)} />
